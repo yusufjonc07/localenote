77f8ac0e31f42599aef3c459b8106f0b6fa53020
yusufjonc07 <ahmedovyusufjon3@gmail.com>
2024-11-10 23:51:42 +0900

    creating http error handler to make our error handling code short and setting via use method in the middleware

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const bodyBarser = require("body-parser");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyBarser.json());
 
-app.use((error, req, res, next) => {
-  // this an example of middleware
+app.get("/", (req, res) => {
+  res.send(`You are sending a request to: ${req.url}`);
 });
 
-app.get("/", (req, res) => {
-    res.send(`You are sending a request to: ${req.url}`)
+/// whenever there is no the requested route, 404 not found error should be thowen
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
+
+app.use((error, req, res, next) => {
+  /// here we are listening for the event that request send from our endpoints.
+  if (res.headerSent) {
+    return next(error);
+  }
+  /// then we see there is error either exist or unexist, 
+  // and we send it user as json with error code specified
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
 });
 
 app.listen(5001);
--- backend/models/http-error.js
+++ backend/models/http-error.js
@@ -1,7 +1,7 @@
 class HttpError extends Error {
     constructor(message, errorCode){
         super(message)
-        self.code = errorCode
+        this.code = errorCode
     }
 }
 
