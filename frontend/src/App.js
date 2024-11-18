ea0fcc60dac8f5c6227f591d6f3305ee7fe297fc
yusufjonc07 <ahmedovyusufjon3@gmail.com>
2024-11-18 22:57:24 +0900

    auto-login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -15,24 +15,39 @@ import Auth from "./user/pages/Auth";
 import { AuthContext } from "./shared/context/auth-context";
 
 const App = () => {
-  const [token, setToken] = useState(false);
+  const [token, setToken] = useState(null);
   const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+    const userDataStr = localStorage.getItem("userData");
+
+    try {
+      const userData = JSON.parse(userDataStr);
+      setToken(userData.token);
+      setUserId(userData.userId);
+    } catch (error) {}
+  }, []);
 
   const login = useCallback((userId, _token) => {
     setToken(_token);
-    setUserId(userId)
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({ userId: userId, token: _token })
+    );
+    setUserId(userId);
   }, []);
 
   const logout = useCallback(() => {
     setToken(null);
-    setUserId(null)
+    localStorage.setItem("userData", null);
+    setUserId(null);
   }, []);
 
   let routes;
   if (token) {
     routes = (
       <Switch>
-         <Route path="/" exact>
+        <Route path="/" exact>
           <Users />
         </Route>
         <Route path="/:userId/places" exact>
@@ -77,9 +92,7 @@ const App = () => {
     >
       <Router>
         <MainNavigation />
-        <main>
-          {routes}
-        </main>
+        <main>{routes}</main>
       </Router>
     </AuthContext.Provider>
   );
